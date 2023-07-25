b87e227ad904f202b34d8094eb35b29f74b7b23c
yakisova41 <yakisoba5emon@gmail.com>
2023-07-25 18:05:09 +0000

    🩹 XのロゴのPathが変わったため修正

--- userscript.user.js
+++ userscript.user.js
@@ -5,7 +5,7 @@
 // @description  Get our Twitter back from Elon.
 // @description:ja  イーロンから私達のTwitterを取り戻します
 // @namespace    https://xtotwitter.yakisova.com
-// @version      1.3.0
+// @version      1.4.0
 // @author       yakisova41
 // @match        https://twitter.com/*
 // @match        https://X.com/*
@@ -15,32 +15,33 @@
 // ==/UserScript==
  
 'use strict';
-const xLogoPath = "M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0"
+const xLogoPath = "M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"
+const loadingXLogoPath = "M14.258 10.152L23.176 0h-2.113l-7.747 8.813L7.133 0H0l9.352 13.328L0 23.973h2.113l8.176-9.309 6.531 9.309h7.133zm-2.895 3.293l-.949-1.328L2.875 1.56h3.246l6.086 8.523.945 1.328 7.91 11.078h-3.246zm0 0"
 const birdPath = "M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z";
 const twitterColor = "rgb(29, 155, 240)";
 const loadingBirdColor = "rgba(29,161,242,1.00)";
- 
+
 const style = document.createElement("style");
- 
+
 style.innerHTML = `
-path[d="${xLogoPath}"]{
+path[d="${xLogoPath}"], path[d="${loadingXLogoPath}"]{
     d:path("${birdPath}");
     fill: inherit;
     color: ${twitterColor};
 }
- 
+
 div[style="color: rgb(239, 243, 244);"] > svg > g > path[d="${xLogoPath}"] {
     color: rgb(239, 243, 244);
 }
- 
+
 div[aria-label="Loading…"] > svg > g > path {
     d:path("${birdPath}");
     fill: inherit;
     color: ${loadingBirdColor};
 }
 `;
- 
- 
+
+
 const i = setInterval(()=>{
     const head = document.head;
     if(head !== null){

7e58a34c0ed7099a95ea4fcebf164f90826797d5
yakisova41 <yakisoba5emon@gmail.com>
2023-07-28 06:13:44 +0000

    ✨ タイトルをXからTwitterへ

--- userscript.user.js
+++ userscript.user.js
@@ -46,4 +46,31 @@ const head = unsafeWindow.document.head;
 if(head !== null){
     unsafeWindow.document.head.querySelector('[rel="shortcut icon"]').href = "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII=";
     head.appendChild(style);
+    titleChange(head);
+}
+
+function titleChange(head) {
+    const i = setInterval(()=>{
+        const titleEl = head.querySelector("title");
+
+        if(titleEl !== null) {
+            const titleOb = new MutationObserver(()=>{
+                if(titleEl.innerHTML === "X") {
+                    titleEl.innerHTML = "Twitter";
+                }
+                else {
+                    const split = titleEl.innerHTML.split("/");
+
+                    if(split[1] === " X"){
+                        split[1] = " Twitter";
+                        titleEl.innerHTML = split.join("/");
+                    }
+                }
+            });
+            titleOb.observe(titleEl, {
+                childList: true
+            });
+            clearInterval(i);
+        }
+    },100);
 }

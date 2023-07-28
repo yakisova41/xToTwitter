1c6c7b9006f134f29ef6885dc8def229baafb489
yakisova41 <yakisoba5emon@gmail.com>
2023-07-28 07:43:15 +0000

    ✨ [Extension Only] PWA X to Twitter icon

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -32,6 +32,7 @@ const i = setInterval(()=>{
         document.head.querySelector('[rel="shortcut icon"]').href = "data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAACXBIWXMAAAsTAAALEwEAmpwYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAJkSURBVHgB7VZBbtpQEH3zIW0WVYuXVaH4Bs0NSk4AOUFhEarskhMknIDsqkKlcIT0BNAT1D1B3ZJK3dmVuirwp/MhVmzAxiagKBJv9+ePZ97M/JkxsMMODwzChlD84FWQp3MxeCDHAhiumB+MJrr1+8Ryw3p/9+H4DctfIPCq49Xlw8Kv99YlMuB19885gy/i7llziwGfFFWJyR02XzSCuwiBUse7BlFVaz5LS8KQVkRXaXRJsqImfDjKSZBNyzEyFWFKVJ4KFbWLElUao6KbSk8i9TXgTPaorxTskPwOxa7/9baGt4zg8oQbNyfWYJlRU0/KUx9ZwNwYNq1ecFRzl18QpW0bB0Ks//KjV1uwlbuLJA3GxEdh5wb5yGEPl3qMd2xecYQHKnlFlVLX95kxYCFKGg5IlU2a0uLpCM68LEJA+sJ/Dm6Jy3aMjQIRakRUm+UuvfOp/X34iQSejeFo0Hdx4optG5uFH/R+GHNvANcm3VtwLs+Lvy2TRwhIOnrYHhysIuDKcCDwGbYAjglOzQt+HssElF6dvoNNOZeuCSbfSgIGMjILMo4/ExZf7TqghNLmlwm1gpSC2tmaLAZMvWGz0Iu7XpqBm2NrQNN5cD+Y5ZOTdZyok3RZMusZOJUN+QZrQFb0oQkG6xIIYHe8A03Unx/Ryd6jS2ctAsbxmFRVynGKlM5na5ePVkUe0p+h9MmraS2zXqYgmSWjOPtElHbLTVB3Q79gqQlMScxqXpeav0UWiGMmXKSNOpZAAPvKs/U/1MRoxRxl+5WD+psUy2D5IdmRVoWjnqDnLlkyO+zwaPAf1zXwZL751PUAAAAASUVORK5CYII=";
         head.appendChild(style);
         titleChange(head);
+        replaceManifest(head);
         clearInterval(i);
     }
 });
@@ -60,4 +61,13 @@ function titleChange(head) {
             clearInterval(i);
         }
     },100);
+}
+
+function replaceManifest(head) {
+    document.querySelector('link[rel="manifest"').remove()
+    const manifestEl = document.createElement('link');
+    manifestEl.setAttribute('rel', 'manifest');
+    manifestEl.setAttribute('crossorigin', 'use-credentials');
+    manifestEl.setAttribute('href', chrome.runtime.getURL("/twitterManifest.json"));
+    head.prepend(manifestEl);
 }
